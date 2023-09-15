220148d82a17bbd8c7faaf96ba53dd40a097ae03
AurelienFT <aurelien.foucault@epitech.eu>
2023-09-16 00:16:39 +0200

    Add skeleton front and back end

--- frontend/README.md
+++ frontend/README.md
@@ -1,2 +1,4 @@
 # Bamboo
 WIP - A community collection of Recipes made with Panda to be eaten by your website
+
+This is my first project using svelte, sveltekit and panda css.
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { css } from 'styled-system/css';
+    import {  hstack} from 'styled-system/patterns'
+    let logo = "/logo.png"
+</script>
+ 
+<div class={hstack({justify: 'space-between', px: '4'})}>
+    <div class={hstack({gap: 2})}>
+        <div
+            class={css({
+                fontSize: '2xl',
+                fontWeight: 'bold'
+            })}
+        >
+            Bamboo
+        </div>
+        <img class={css({width: 8 })} src={logo} alt="Bamboo logo" />
+    </div>
+    <button>
+        Connect with github
+    </button>
+</div>
+<h1>Feed your Panda with these recipes</h1>
+<input placeholder="Search your recipe"/>
