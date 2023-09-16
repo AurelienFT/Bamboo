6081ec06f327f5d73c657fc5111af404c2ae38dc
AurelienFT <aurelien.foucault@epitech.eu>
2023-09-17 00:19:23 +0200

    add colors and dark mode

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
+    import SvelteTheme from 'svelte-themes/SvelteTheme.svelte';
     import '../app.css'
 </script>
 
+<SvelteTheme/>
 <slot/>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
 	import { css } from 'styled-system/css';
     import {  hstack} from 'styled-system/patterns'
+	import { afterUpdate } from 'svelte';
+    import themeStore, { setTheme } from 'svelte-themes/themeStore';
     let logo = "/logo.png"
 </script>
  
-<div class={hstack({justify: 'space-between', px: '4'})}>
+<div class={hstack({justify: 'space-between', bg: "bg.inverted", px: '4'})}>
     <div class={hstack({gap: 2})}>
         <div
             class={css({
@@ -18,6 +20,9 @@
     </div>
     <button>
         Connect with github
+    </button>
+    <button on:click={() => $themeStore.theme == 'light' ? setTheme('dark'): setTheme('light')}>
+        {$themeStore.theme ? $themeStore.theme == 'light' ? 'Dark': 'Light': ''}
     </button>
 </div>
 <h1>Feed your Panda with these recipes</h1>
